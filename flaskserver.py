12039385059485695e8e222bf813a2627d5d2cb6
Pravinth Samuel Vethanayagam <pravinth.samuel@gmail.com>
2016-04-06 11:40:49 +0100

    Just a sample flask hello world application

---
+++ flaskserver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 11:37:13 2016
+
+@author: pravinth
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
